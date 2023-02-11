dec12ffe64bfd84cf186e902625fa031ecd7889a
mhmudalpurd <mahmoudalipour.d@gmail.com>
2023-02-12 00:01:17 +0100

    before starting Type conversion

---
+++ 01_python/01_basics/01_basics.py
@@ -0,0 +1,83 @@
+#identation
+
+age = 8
+
+if(age < 5):
+    print('He/She is a kid')
+
+
+#Single Line Comment
+
+'''
+Multi 
+Line
+Comments!
+OR 
+DocString!
+'''
+
+#Continuaton of statements
+a = True
+b = False
+c = True
+if (a==True) and (b==False) \
+    and (c==True):
+        print('back slash')
+        
+        
+#identifiers
+variable1 = 2
+_variable0 = 'Filoger'
+
+#  1variable = True --> identifier can not starts with digits!
+
+# @variable = 7 --> identifiers can not start with special characters!
+
+# Var != var --> python is case sensitive!
+
+#Object ( Scalar & Non-Scalar)
+
+var_one = 17 #---> type:int
+print('type of var_one: ' + str(type(var_one)))
+
+
+var_two = 19.31 #---> type:float
+print('type of var_two: ' + str(type(var_two)))
+
+var_three = True # type:boolean
+print('type of var_three: ' + str(type(var_three)))
+
+var_four = 'TRUE' #type: string
+print('type of var_four: ' + str(type(var_four)))
+
+#Constant in python (constant does not support in python)
+
+NUMBER_OF_STUDENTS = 30.0
+
+
+#Cast (Type Conversion!)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+    
+    
