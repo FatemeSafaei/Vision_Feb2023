6ef50a23bd98faafa17633eb346a019d3168f5c2
mhmudalpurd <mahmoudalipour.d@gmail.com>
2023-02-12 17:51:05 +0100

    identatin, continouation

---
+++ 01_python/01_basics/01_basics.py
@@ -0,0 +1,27 @@
+#identation
+age = 4
+
+if(age < 5):    
+    print('He/She is a kid')
+    
+    
+#Single line comment
+
+'''
+this is about the 
+basics in python!
+and first session!
+'''
+a = True
+b = False
+c = True
+
+if (a==True) and (b==False) \
+    and (c==True):
+        print('BackSlash')
+
+
+
+
+
+
