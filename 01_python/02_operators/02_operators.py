1875e77d9314dc430e69c8ac242ca0e52db7e535
mhmudalpurd <mahmoudalipour.d@gmail.com>
2023-02-14 12:20:21 +0100

    Arithmetic Ops. Completed!

--- 01_python/02_operators/02_operators.py
+++ 01_python/02_operators/02_operators.py
@@ -1,1 +1,44 @@
-#operators 
+#Arithmetic Ops. 
+
+a = 20
+b = 15
+
+## Addition
+print("a+b: ", a+b)
+
+## Subtraction
+print("a-b: ", a - b)
+
+## Multiplication
+print('a x b: ', a*b)
+
+
+## Division
+print('a / b:', a / b)
+
+
+## Power
+print('a ** b: ', a ** b)
+
+
+## Floor Division
+print('a // b: ', a // b )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
