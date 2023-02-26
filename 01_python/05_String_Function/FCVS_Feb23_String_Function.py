0057ed9bff505e11abedb966ff9e9c4937a2664b
mhmudalpurd <mahmoudalipour.d@gmail.com>
2023-02-26 17:00:58 +0100

    end of the function

---
+++ 01_python/05_String_Function/FCVS_Feb23_String_Function.py
@@ -0,0 +1,63 @@
+#Function
+def greet():
+    """
+    the function prints Hi!
+    """
+    print('Hi')
+
+
+greet()
+#greet()
+#greet()
+
+def greet(name):
+    print(f'Hi {name}')
+
+
+
+greet('Asghar')
+
+def greet(first_name, last_name):
+    return f'Hello {first_name} {last_name}'
+
+g = greet('Ali', 'Alavi')
+out = g + ' welcome!'
+#print(type(g))
+print(out)
+
+
+# open the camera! --> func_1
+# take a quick photo! --> func_2
+# recognize the guest! --> func_3
+# say hello to the guest! --> func_4
+
+#def welcome():
+    #func_1
+    #func_2
+    #....
+    
+# Class!
+# utils!
+## calculate_area!
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
