04cb8ea153a2935abe4e7ab71bedc11d33f9ebf7
mhmudalpurd <mahmoudalipour.d@gmail.com>
2023-03-10 17:01:50 +0330

    func no class

---
+++ 01_python/10_oop/func_no_cls/call_func_no_cls.py
@@ -0,0 +1,14 @@
+#from file_path import function!
+
+from func_no_cls import add, subtract, multiply, power, divide, square_root, logarithm
+
+num1 = 10
+num2 = 5
+
+print('Addition: ', add(num1, num2))
+print('Subtraction: ', subtract(num1, num2))
+print('Multiplicatin: ', multiply(num1, num2))
+print('Division: ', divide(num1, num2))
+print('Power: ', power(num1, num2))
+print('Square Root: ', square_root(num1))
+print('logarithm: ', logarithm(num1, num2))
